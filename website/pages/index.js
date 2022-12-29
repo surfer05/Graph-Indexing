e3301d6201c215664cb816868c707c5fd2dc6db8
surfer05 <rahul05alok@gmail.com>
2022-12-29 15:00:46 +0530

    Some Changes

--- website/pages/index.js
+++ website/pages/index.js
@@ -310,7 +310,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>LW3Punks</title>
+        <title>Lottery Game by Surfer</title>
         <meta name="description" content="LW3Punks-Dapp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
